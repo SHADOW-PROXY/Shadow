fd05e5ba581b97b61c78af7b9f9a03c1798573a4
rift <117926989+slushzies@users.noreply.github.com>
2023-03-12 16:19:08 -0500

    🌙Force Dark Mode Update

--- static/script.js
+++ static/script.js
@@ -1,3 +1,52 @@
+function darkMode() {
+  const body = document.querySelector('body');
+  const bgColor = window.getComputedStyle(body).getPropertyValue('background-color');
+
+  // Check if background color is dark
+  function isColorDark(color) {
+    const rgb = color.match(/\d+/g).map(Number);
+    const brightness = (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000;
+    return brightness < 128;
+  }
+
+  if (isColorDark(bgColor)) {
+    console.log('website is in dark mode');
+  } else {
+    console.log('website is not in dark mode');
+    function invert(str,type){
+      var t1 = str.split("(");
+      var t2 = t1[1].split(")");
+      var t3 = t2[0].split(",");
+      t3.forEach(function(v,i){
+       if(i<3){
+        if(type=="color")
+            t3[i] = (255 - parseInt(v))<50?120:(255 - parseInt(v));
+        else
+            t3[i] = (255 - parseInt(v));
+       }
+      })
+      t3 = t3.join(",");
+      return t1[0]+"("+t3+")";
+    }
+    //invert color and backgroundcolor of every dom node
+    document.querySelectorAll('*:not([invTouch])').forEach(function(node) {
+        var style  = window.getComputedStyle(node);
+        node.style.backgroundColor = invert(style.backgroundColor,"back");
+        node.style.color = invert(style.color,"color");
+        node.setAttribute("invTouch", "true");
+    });
+  }
+}
+
+function myFunction() {
+  alert("Hello World!");
+}
+
+
+
+
+
+
 function byId(id) {
   return document.getElementById(id);
 }
@@ -80,8 +129,12 @@ function load() {
   }
 
   iframe.addEventListener('load', function() {
+    const iframeWindow = iframe.contentWindow;
     var title = iframe.contentDocument.title || "Welcome";
     byId('tabb'+currentTab).innerHTML = title + ' <span class="topright2" onclick="closeTab(' + currentTab + ')">X</span>';
+    var functionCode = darkMode.toString();
+    // Execute the code in the iframe's context
+    iframeWindow.eval("(" + functionCode + ")()");
   });
 }
 
@@ -180,3 +233,5 @@ function isUrl() {
     if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
     return false;
 }
+
+
